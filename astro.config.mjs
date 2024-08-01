3e59a3bcc3961000d7b1be04304d6856245c4f56
santigonzalezla <santigonzalezla@gmail.com>
2024-08-01 15:50:59 -0500

    First push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import icon from "astro-icon";
-
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
